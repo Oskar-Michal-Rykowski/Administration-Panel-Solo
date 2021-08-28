6db563d2a9015dfafb16ff9263a5f3dac2fd8947
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-08-28 18:57:44 +0200

    Add showing pages in js

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+import { select, classNames } from './settings.js';
+
+class showingPages {
+  constructor() {
+    const thisApp = this;
+
+    thisApp.getElements();
+    thisApp.initActions();
+  }
+
+  getElements() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelectorAll(select.pages);
+    console.log('thisApp.pages', thisApp.pages);
+
+    thisApp.menuLinks = document.querySelectorAll(select.menuLinks);
+    console.log('thisApp.menuLinks', thisApp.menuLinks);
+  }
+
+  initActions() {
+    const thisApp = this;
+
+    //find links in menu and add clickListener
+    for (let link of thisApp.menuLinks) {
+      console.log('link', link);
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log('addEventListener added');
+        for (let page of thisApp.pages) {
+          page.classList.remove(classNames.elementStatus.activePage);
+          console.log('class removed');
+        }
+
+        const linksHref = link.getAttribute('href');
+        console.log('linksHref', linksHref);
+
+        const pageId = linksHref + '-page';
+        console.log('pageId', pageId);
+
+        const page = document.getElementById(pageId);
+        console.log('page', page);
+
+        page.classList.add(classNames.elementStatus.activePage);
+      });
+    }
+
+    //after click find all pages and remove class active-page
+
+    //from clicked element get attribute href
+
+    //find element with id = above const + '-page'
+
+    //add class active page
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const app = new showingPages();
---
+++ src/js/settings.js
@@ -0,0 +1,10 @@
+export const select = {
+  pages: '.page',
+  menuLinks: '.bullet',
+};
+
+export const classNames = {
+  elementStatus: {
+    activePage: 'active-page',
+  },
+};
