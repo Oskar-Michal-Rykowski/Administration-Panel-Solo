52a6268e83dea786300f9e57893cb1157c81c042
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-08-29 18:20:35 +0200

    Add popups functionality in JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,22 @@
 import { select, classNames } from './settings.js';
 
-class navigationWork {
+class actionStart {
   constructor() {
     const thisApp = this;
 
     thisApp.getElements();
-    thisApp.initActions();
+    thisApp.initMenuActions();
+    thisApp.initPopupsActions();
   }
 
   getElements() {
     const thisApp = this;
 
     thisApp.pages = document.querySelectorAll(select.pages);
-    console.log('thisApp.pages', thisApp.pages);
+    // //console.log('thisApp.pages', thisApp.pages);
 
     thisApp.menuLinks = document.querySelectorAll(select.menuLinks);
-    console.log('thisApp.menuLinks', thisApp.menuLinks);
+    //console.log('thisApp.menuLinks', thisApp.menuLinks);
 
     //Find hamburger
     thisApp.hamburger = document.querySelector(select.hamburger);
@@ -24,33 +25,44 @@ class navigationWork {
     thisApp.navigationDropdown = document.querySelector(
       select.navigationDropdown
     );
+
+    thisApp.popupOverlay = document.getElementById(select.overlay);
+
+    thisApp.popupMessage = document.querySelector(select.popupMessage);
+    thisApp.yourManager = document.querySelector(select.yourManager);
+
+    thisApp.popupLogin = document.querySelector(select.popupLogin);
+    thisApp.userProfile = document.querySelector(select.userProfile);
+
+    thisApp.popupQuit = document.querySelector(select.popupQuit);
+    thisApp.quitButton = document.querySelector(select.quitButton);
   }
 
-  initActions() {
+  initMenuActions() {
     const thisApp = this;
 
     //find links in menu and add eventListener
     for (let link of thisApp.menuLinks) {
-      console.log('link', link);
+      //console.log('link', link);
       link.addEventListener('click', function (event) {
         event.preventDefault();
-        console.log('addEventListener added');
+        //console.log('addEventListener added');
         //after click find all pages and remove class active-page
         for (let page of thisApp.pages) {
           page.classList.remove(classNames.elementStatus.activePage);
-          console.log('class removed');
+          //console.log('class removed');
         }
 
         //from clicked element get attribute href
         const linksHref = link.getAttribute('href');
-        console.log('linksHref', linksHref);
+        //console.log('linksHref', linksHref);
 
         //find element with id = above const + '-page'
         const pageId = linksHref + '-page';
-        console.log('pageId', pageId);
+        //console.log('pageId', pageId);
 
         const page = document.getElementById(pageId);
-        console.log('page', page);
+        //console.log('page', page);
 
         //add class active page
         page.classList.add(classNames.elementStatus.activePage);
@@ -59,7 +71,7 @@ class navigationWork {
       });
     }
 
-    //Hamburger working
+    //HAMBURGER WORKING
     //Function
     function navigationDropdown() {
       thisApp.navigationDropdown.classList.toggle(
@@ -74,7 +86,72 @@ class navigationWork {
     });
     //if clicked toggle class active
   }
+
+  initPopupsActions() {
+    const thisApp = this;
+
+    function closeModal() {
+      thisApp.popupOverlay.classList.remove(
+        classNames.elementStatus.overlayShow
+      );
+    }
+
+    document
+      .querySelectorAll(select.closeButtonsOverlay)
+      .forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+          e.preventDefault();
+          closeModal();
+        });
+      });
+
+    document
+      .querySelector(select.overlayId)
+      .addEventListener('click', function (e) {
+        if (e.target === this) {
+          closeModal();
+        }
+      });
+
+    document.addEventListener('keyup', function (e) {
+      if (e.keyCode === 27) {
+        closeModal();
+      }
+    });
+
+    function openModal(modal) {
+      console.log('modal', modal);
+
+      document.querySelectorAll('#overlay > *').forEach(function (modal) {
+        modal.classList.remove(classNames.elementStatus.overlayShow);
+      });
+
+      document
+        .querySelector(select.overlayId)
+        .classList.add(classNames.elementStatus.overlayShow);
+
+      modal.classList.add(classNames.elementStatus.overlayShow);
+    }
+
+    thisApp.yourManager.addEventListener('click', function (event) {
+      event.preventDefault();
+      console.log('thisApp.popupMessage', thisApp.popupMessage);
+      openModal(thisApp.popupMessage);
+    });
+
+    thisApp.userProfile.addEventListener('click', function (event) {
+      event.preventDefault();
+      console.log('thisApp.popupMessage', thisApp.popupMessage);
+      openModal(thisApp.popupLogin);
+    });
+
+    thisApp.quitButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      console.log('thisApp.popupMessage', thisApp.popupMessage);
+      openModal(thisApp.popupQuit);
+    });
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
-const app = new navigationWork();
+const app = new actionStart();
--- src/js/settings.js
+++ src/js/settings.js
@@ -3,11 +3,21 @@ export const select = {
   menuLinks: '.bullet',
   hamburger: '.menu-button',
   navigationDropdown: '.navigation-dropdown',
+  overlay: 'overlay',
+  overlayId: '#overlay',
+  closeButtonsOverlay: '#overlay .js--close-modal',
+  popupMessage: '.popup-message',
+  yourManager: '.your-manager-profile',
+  popupLogin: '.popup-login',
+  userProfile: '.upper-account',
+  popupQuit: '.popup-quit',
+  quitButton: '.upper-logout',
 };
 
 export const classNames = {
   elementStatus: {
     activePage: 'active',
     activeMenu: 'active',
+    overlayShow: 'show',
   },
 };
