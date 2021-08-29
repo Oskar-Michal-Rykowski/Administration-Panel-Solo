27dcfed03ad71c0a97adbf56786666634e5b7646
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-08-29 09:23:31 +0200

    Put js comments in right place

--- src/js/script.js
+++ src/js/script.js
@@ -21,37 +21,33 @@ class navigationWork {
   initActions() {
     const thisApp = this;
 
-    //find links in menu and add clickListener
+    //find links in menu and add eventListener
     for (let link of thisApp.menuLinks) {
       console.log('link', link);
       link.addEventListener('click', function (event) {
         event.preventDefault();
         console.log('addEventListener added');
+        //after click find all pages and remove class active-page
         for (let page of thisApp.pages) {
           page.classList.remove(classNames.elementStatus.activePage);
           console.log('class removed');
         }
 
+        //from clicked element get attribute href
         const linksHref = link.getAttribute('href');
         console.log('linksHref', linksHref);
 
+        //find element with id = above const + '-page'
         const pageId = linksHref + '-page';
         console.log('pageId', pageId);
 
         const page = document.getElementById(pageId);
         console.log('page', page);
 
+        //add class active page
         page.classList.add(classNames.elementStatus.activePage);
       });
     }
-
-    //after click find all pages and remove class active-page
-
-    //from clicked element get attribute href
-
-    //find element with id = above const + '-page'
-
-    //add class active page
   }
 }
 
