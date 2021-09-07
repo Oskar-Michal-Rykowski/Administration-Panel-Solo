b55e8551d5355b39dedec0825c38174e5447b524
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-07 17:11:11 +0200

    Add docs

---
+++ docs/js/settings.js
@@ -0,0 +1,23 @@
+export const select = {
+  pages: '.page',
+  menuLinks: '.bullet',
+  hamburger: '.menu-button',
+  navigationDropdown: '.navigation-dropdown',
+  overlay: 'overlay',
+  overlayId: '#overlay',
+  closeButtonsOverlay: '#overlay .js--close-modal',
+  popupMessage: '.popup-message',
+  yourManager: '.your-manager-profile',
+  popupLogin: '.popup-login',
+  userProfile: '.upper-account',
+  popupQuit: '.popup-quit',
+  quitButton: '.upper-logout',
+};
+
+export const classNames = {
+  elementStatus: {
+    activePage: 'active',
+    activeMenu: 'active',
+    overlayShow: 'show',
+  },
+};
